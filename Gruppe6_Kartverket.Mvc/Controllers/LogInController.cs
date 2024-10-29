2ef457b2413bfe7860463cb744b71cfc683a96ea
Kata-Loore Tamm <144672861+KataLoore@users.noreply.github.com>
2024-10-29 15:14:50 +0100

    Førsteutkast for login & registrering
    Lagde LogIn mappe under views, samt LogInController. Lagde views til registrering og login under LogIn mappe i Views, i tillegg til registrationform-model.

--- Gruppe6_Kartverket.Mvc/Controllers/LogInController.cs
+++ Gruppe6_Kartverket.Mvc/Controllers/LogInController.cs
@@ -1,12 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
+using Gruppe6_Kartverket.Mvc.Models;
+using System.Diagnostics;
 
 namespace Gruppe6_Kartverket.Mvc.Controllers;
 
 public class LogInController : Controller
 {
-    // GET
-    public IActionResult Index()
+    [HttpGet]
+    public IActionResult LogInPage()
     {
         return View();
     }
+
+    [HttpGet]
+    public IActionResult RegistrationForm()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public ViewResult RegistrationForm(RegistrationFormModel userData)
+    {
+        return View("Overview", userData);
+    }
+    
+
+    //[HttpPost]
+    //public IActionResult RegistrationForm(UserInfo userInfo)
+    //{
+    //    if (!ModelState.IsValid)
+    //        return View(userInfo);
+    //    return RedirectToAction("Index", "Home");
+    //}
 }
