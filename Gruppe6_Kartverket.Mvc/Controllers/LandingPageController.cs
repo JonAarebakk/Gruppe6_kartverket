32bb528c7e5ac69525a8e712fbb4dd3fcce55fca
stinest-uia <144122429+stinest-uia@users.noreply.github.com>
2024-10-30 12:44:38 +0100

    Added views and controllers
    Added empty views and controllers like shown in our navigation map. Uploaded so that the group can add and edit the pages to better simulate actual navigation and functionality.

---
+++ Gruppe6_Kartverket.Mvc/Controllers/LandingPageController.cs
@@ -0,0 +1,21 @@
+using Gruppe6_Kartverket.Mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Gruppe6_Kartverket.Mvc.Controllers
+{
+    public class LandingPageController : Controller
+    {
+        // GET: LandingPage
+        [HttpGet]
+        public IActionResult LandingPage()
+        {
+            return View("LandingPage");
+        }
+        
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
