7d02915f48007a2177de2ccb420b9288e53bf84a
stinest-uia <144122429+stinest-uia@users.noreply.github.com>
2024-11-03 19:04:08 +0100

    Created UserPage and Case page
    Created UserPage and Case(View, Model and Controller). And made some changes to the site.css, login.css, layout.cshtml. And put the logo in the navbar

--- Gruppe6_Kartverket.Mvc/Controllers/CaseController.cs
+++ Gruppe6_Kartverket.Mvc/Controllers/CaseController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Gruppe6_Kartverket.Mvc.Models;
 
 namespace Gruppe6_Kartverket.Mvc.Controllers;
 
@@ -7,6 +8,17 @@ public class CaseController : Controller
     // GET
     public IActionResult CaseView()
     {
-        return View();
+        var model = new CaseInfoModel();
+        return View("CaseView", model);
+    }
+    
+    public IActionResult CaseViewWithModel(CaseInfoModel model)
+    {
+        if(!ModelState.IsValid)
+        {
+            return View("CaseView", model);
+        }
+        
+        return View("CaseView", model);
     }
 }
--- Gruppe6_Kartverket.Mvc/Controllers/UserPageController.cs
+++ Gruppe6_Kartverket.Mvc/Controllers/UserPageController.cs
@@ -1,12 +1,46 @@
+
 using Microsoft.AspNetCore.Mvc;
+using Gruppe6_Kartverket.Mvc.Models;
 
 namespace Gruppe6_Kartverket.Mvc.Controllers;
 
 public class UserPageController : Controller
 {
-    // GET
     public IActionResult Index()
     {
-        return View();
+        var model = new UserPageModel
+        {
+            UserName = "User Name",
+            NewMessagesCount = 5,
+            Cases = GetCases() // Assuming GetCases is a method that retrieves cases for the user
+        };
+        return View(model);
+    }
+
+    public IActionResult Settings()
+    {
+        // Logic to load settings
+        return View("Settings");
+    }
+
+    public IActionResult Profile()
+    {
+        // Logic to load user profile
+        return View("Profile");
+    }
+
+    public IActionResult Logout()
+    {
+        // Logic to handle logout
+        return RedirectToAction("Index", "Home");
+    }
+
+    private List<Case> GetCases()
+    {
+        return new List<Case>
+        {
+            new Case { Id = 1, Title = "Case 1", Date = DateTime.Now, Status = "Open" },
+            new Case { Id = 2, Title = "Case 2", Date = DateTime.Now, Status = "Closed" }
+        };
     }
 }
