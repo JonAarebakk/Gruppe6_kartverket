84b27c9f3ca028a4a0cbf07f5bdfd67672f48d00
Jørgen Ege <jorgee17@uia.no>
2024-11-10 03:59:01 +0100

    Database update 2
    Added models for all tables
    Created new builder method in ApplicationDbContext that ties all tables together.
    Specified Primary Keys in each table.

---
+++ Gruppe6_Kartverket.Mvc/Models/Database/UserInfo.cs
@@ -0,0 +1,30 @@
+﻿namespace Gruppe6_Kartverket.Mvc.Models
+{
+    public enum Gender
+    {
+        M, F, O
+    }
+
+    public enum UserStatus
+    {
+        Active, Inactive
+    }
+
+
+    public class UserInfo
+
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public Gender? Gender { get; set; }
+        public UserStatus? UserStatus { get; set; }
+
+
+        //Navigation property to User
+        public virtual User User { get; set; }
+    }
+}
