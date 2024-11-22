ee4689cf9d138f1e688e9817ebfcfc6872185922
JonAarebakk <144025141+JonAarebakk@users.noreply.github.com>
2024-11-22 07:36:32 +0100

    yeah

---
+++ init.sql
@@ -0,0 +1,13 @@
+﻿﻿-- Check if the user 'root'@'%' exists
+SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = 'root' AND host = '%') INTO @user_exists;
+
+-- If the user exists, update the password; otherwise, create the user
+IF @user_exists THEN
+    ALTER USER 'root'@'%' IDENTIFIED BY 'passord123';
+ELSE
+    CREATE USER 'root'@'%' IDENTIFIED BY 'passord123';
+END IF;
+
+-- Grant necessary privileges to the user
+GRANT ALL PRIVILEGES ON kartverket_tables.* TO 'root'@'%';
+FLUSH PRIVILEGES;
