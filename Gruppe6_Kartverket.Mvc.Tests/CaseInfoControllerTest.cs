144a29c5f99de2de0815d711d20b01f12ec5a717
Bulldog2910 <mat.nik.jorgensen@gmail.com>
2024-11-24 12:24:16 +0100

    fix igjen

--- Gruppe6_Kartverket.Mvc.Tests/CaseInfoControllerTest.cs
+++ Gruppe6_Kartverket.Mvc.Tests/CaseInfoControllerTest.cs
@@ -5,7 +5,6 @@ using Gruppe6_Kartverket.Mvc.Models.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using NSubstitute;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -17,7 +16,11 @@ namespace Gruppe6_Kartverket.Mvc.Tests.ControllerTests
         public async Task CaseInfo_InvalidModel_ReturnsNotFound()
         {
             // Arrange
-            var controller = GetUnitUnderTest();
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+            var context = new ApplicationDbContext(options);
+            var controller = new CaseInfoController(context);
             var caseRecordId = 1;
 
             // Act
@@ -31,16 +34,20 @@ namespace Gruppe6_Kartverket.Mvc.Tests.ControllerTests
         public async Task CaseInfo_ValidModel_ReturnsViewResultWithModel()
         {
             // Arrange
-            var controller = GetUnitUnderTest();
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+            var context = new ApplicationDbContext(options);
             var caseRecordId = 1;
             var caseRecord = new CaseRecord
             {
                 CaseRecordId = caseRecordId,
                 CaseTitle = "Test Case"
             };
+            context.CaseRecords.Add(caseRecord);
+            context.SaveChanges();
 
-            var dbContext = Substitute.For<ApplicationDbContext>();
-            dbContext.CaseRecords.FindAsync(caseRecordId).ReturnsForAnyArgs(new ValueTask<CaseRecord>(caseRecord));
+            var controller = new CaseInfoController(context);
 
             // Act
             var result = await controller.CaseInfo(caseRecordId);
@@ -49,14 +56,6 @@ namespace Gruppe6_Kartverket.Mvc.Tests.ControllerTests
             var viewResult = Assert.IsType<ViewResult>(result);
             var model = Assert.IsType<CaseDetailsViewModel>(viewResult.Model);
             Assert.Equal(caseRecordId, model.CaseRecordId);
-        }
-
-        private CaseInfoController GetUnitUnderTest()
-        {
-            var dbContext = Substitute.For<ApplicationDbContext>();
-            var controller = new CaseInfoController(dbContext);
-            controller.ControllerContext.HttpContext = new DefaultHttpContext();
-            return controller;
         }
     }
 }
